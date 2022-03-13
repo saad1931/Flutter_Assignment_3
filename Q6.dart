69822dcd7fff560c9502ef5d143d0676669e01e5
Muhammad Saad <MuhammadSaad3119@gmail.com>
2022-03-13 12:47:30 +0500

    Flutter Assignment 3

--- Q6.dart
+++ Q6.dart
@@ -3,7 +3,9 @@ void main()
 print("Question 6");
 var arr1=[7,14,21,28,35,42,49,56,63,70];
 var arr2=[1,2,3,4,5,6,7,8,9,10];
+var dt=Map.fromIterables(arr2,arr1);
 print(arr1);
 print(arr2);
-
+print("Table of 7:");
+print(dt);
 }
